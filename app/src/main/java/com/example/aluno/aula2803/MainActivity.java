076031bf2c7f91330f6134798648b70f8748ac5f
alesonteruji <alesonteruji@gmail.com>
2018-03-28 21:42:50 -0300

    Inicial

--- app/src/main/java/com/example/aluno/aula2803/MainActivity.java
+++ app/src/main/java/com/example/aluno/aula2803/MainActivity.java
@@ -4,13 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ListView listViewTimes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        listViewTimes = (ListView) findViewById(R.id.listTimes);
+
+        String times[] = {"Flamengo", "ACP", "Inter", "Avai"};
+
+        ArrayAdapter<String> adapterTimes = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, times);
+
+
+        listViewTimes.setAdapter(adapterTimes);
+
+
     }
 
 
